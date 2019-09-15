6ae041a5d103ffcb90db35377b54fe862431bd44
Czarek <sarek123456789@gmail.com>
2019-09-15 11:02:53 +0200

    Implement bubble sort

--- main.go
+++ main.go
@@ -4,6 +4,33 @@ import (
 	"fmt"
 )
 
+func swap(slice []int, index1 int, index2 int) []int {
+	firstVal := slice[index1]
+	secondVal := slice[index2]
+
+	slice[index1] = secondVal
+	slice[index2] = firstVal
+
+	return slice
+}
+
+func bubbleSort(toSort []int) []int {
+
+	for i := len(toSort) - 1; i != 0; i-- {
+		for j := 0; j < i; j++ {
+			nextIndex := j + 1
+
+			if nextIndex < len(toSort) && toSort[j] > toSort[nextIndex] {
+				toSort = swap(toSort, j, nextIndex)
+			}
+		}
+	}
+
+	return toSort
+}
+
 func main() {
-	fmt.Println("Hello world")
+	toSort := []int{5, 9, 2, 7, 1}
+	sorted := bubbleSort(toSort)
+	fmt.Println(sorted)
 }
